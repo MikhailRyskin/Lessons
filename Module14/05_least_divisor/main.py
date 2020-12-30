46465e5980733bba4932521f7ceca00de9fe276f
Mikhail <rymv@yandex.ru>
2020-12-30 12:04:05 +0300

    исправил задачи 5 и 7

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,12 +1,14 @@
 import math
 
-# TODO, пожалуйста, реализуйте функцией.
-#  " Напишите функцию, которая находит его наименьший делитель, отличный от 1."
+
+def min_divider(number):
+    min_divider = number
+    for k in range(2, int(math.sqrt(number)) + 1):
+        if n % k == 0:
+            min_divider = k
+            break
+    return min_divider
+
 
 n = int(input('Введите число: '))
-min_divider = n
-for k in range(2, int(math.sqrt(n)) + 1):
-    if n % k == 0:
-        min_divider = k
-        break
-print('Наименьший делитель, отличный от единицы:', min_divider)
+print('Наименьший делитель, отличный от единицы:', min_divider(n))
--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,39 +1,52 @@
+# def three_of_four(n):
+#     dig1 = n // 1000
+#     dig2 = (n // 100) % 10
+#     dig3 = (n // 10) % 10
+#     dig4 = n % 10
+#     sample = dig1
+#     count = 1
+#     if dig2 == sample:
+#         count += 1
+#     if dig3 == sample:
+#         count += 1
+#     if dig4 == sample:
+#         count += 1
+#     if count == 3:
+#         return True
+#     else:
+#         dig1, dig2 = dig2, dig1
+#         sample = dig1
+#         count = 1
+#         if dig2 == sample:
+#             count += 1
+#         if dig3 == sample:
+#             count += 1
+#         if dig4 == sample:
+#             count += 1
+#         if count == 3:
+#             return True
+#         else:
+#             return False
 
-# TODO, давайте подумаем, как сократить количество условных операторов в коде
-def three_offour(n):
-    dig1 = n // 1000
-    dig2 = (n // 100) % 10
-    dig3 = (n // 10) % 10
-    dig4 = n % 10
-    sample = dig1
-    count = 1
-    if dig2 == sample:
-        count += 1
-    if dig3 == sample:
-        count += 1
-    if dig4 == sample:
-        count += 1
-    if count == 3:
-        return True
-    else:
-        dig1, dig2 = dig2, dig1
-        sample = dig1
-        count = 1
-        if dig2 == sample:
-            count += 1
-        if dig3 == sample:
-            count += 1
-        if dig4 == sample:
-            count += 1
+def three_of_four(year):
+    sample = year[0]
+    second = False
+    for k in range(2):
+        if second:
+            sample = year[1]
+        count = 0
+        for symbol in year:
+            if symbol == sample:
+                count += 1
         if count == 3:
             return True
-        else:
-            return False
+        second = True
+    return False
 
 
 a = int(input('Введите первый год: '))
 b = int(input('Введите второй год: '))
 print('Года от', a, 'до', b, 'с тремя одинаковыми цифрами:')
 for year in range(a, b + 1):
-    if three_offour(year):
+    if three_of_four(str(year)):
         print(year)
