1bd7e4a1128e8fa7b12725f114fbe57885006720
Mikhail <rymv@yandex.ru>
2021-01-21 17:59:47 +0300

    HW16_5

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -8,6 +8,13 @@ violator_songs = [
     ['Policy of Truth', 4.76],
     ['Blue Dress', 4.29],
     ['Clean', 5.83]
-]
-
-# TODO здесь писать код
+    ]
+number_songs = int(input('Сколько песен выбрать? '))
+total_duration = 0
+for num in range(number_songs):
+    print('Название', num + 1, 'песни:', end=' ')
+    song = input('')
+    for ind in range(len(violator_songs)):
+        if violator_songs[ind][0] == song:
+            total_duration += violator_songs[ind][1]
+print('Общее время звучания песен:', round(total_duration, 2), 'минут')
