200e20b50b3cc17e17aefba5e57b3ae1d38993cc
Mikhail <rymv@yandex.ru>
2021-01-21 15:28:28 +0300

    HW16_4

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,31 @@
+def if_exist(guest, guests_list):
+    for element in guests_list:
+        if element == guest:
+            return True
+    return False
+
+
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+while True:
+    print('Сейчас на вечеринке', len(guests), 'человек:', guests)
+    action = input('Гость пришел или ушел? ')
+    if action == 'Пора спать':
+        print('Вечеринка закончилась, все легли спать.')
+        break
+    elif action == 'ушел':
+        guest = input('Имя гостя: ')
+        if if_exist(guest, guests):
+            print('Пока,', guest, '!')
+            guests.remove(guest)
+        else:
+            print('Нет такого гостя.')
+    elif action == 'пришел':
+        guest = input('Имя гостя: ')
+        if len(guests) < 6:
+            print('Привет,', guest, '!')
+            guests.append(guest)
+        else:
+            print('Прости,', guest, ', но мест нет.')
+    else:
+        print('Неверный ввод!')
