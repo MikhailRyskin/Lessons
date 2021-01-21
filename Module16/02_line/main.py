00b54911f6343a1a10b7b8b87cf9411b2203b967
Mikhail <rymv@yandex.ru>
2021-01-21 12:49:18 +0300

    HW16_2

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def sort_choice(my_list):
+    for i_mn in range(len(my_list)):
+        for i_curr in range(i_mn, len(my_list)):
+            if my_list[i_curr] < my_list[i_mn]:
+                my_list[i_curr], my_list[i_mn] = my_list[i_mn], my_list[i_curr]
+
+
+class_1 = list(range(160, 177, 2))
+class_2 = list(range(162, 181, 3))
+class_1.extend(class_2)
+sort_choice(class_1)
+print('Отсортированный список:', class_1)
