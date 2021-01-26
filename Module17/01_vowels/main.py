d29bfd867255ee0f9ff7a4cc0479f75a17862c20
Mikhail <rymv@yandex.ru>
2021-01-26 18:02:56 +0300

    HW17_1
    Signed-off-by: Mikhail <rymv@yandex.ru>

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+vowels = 'ауоыиэяюёе'
+text = input('Введите текст: ')
+vowels_list = [letter for letter in text if vowels.find(letter) != -1]
+print('Список гласных букв:', vowels_list)
+print('Длина списка:', len(vowels_list))
