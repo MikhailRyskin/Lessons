7602dd0938e1f02a32786a305eeff34115ce44ea
Mikhail <rymv@yandex.ru>
2021-01-26 19:20:17 +0300

    HW17_3
    Signed-off-by: Mikhail <rymv@yandex.ru>

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import random
+
+team_1 = [round((random.random() * (10 - 5) + 5), 2) for _ in range(20)]
+team_2 = [round((random.random() * (10 - 5) + 5), 2) for _ in range(20)]
+winner_list = [(team_1[ind] if team_1[ind] > team_2[ind] else team_2[ind]) for ind in range(20)]
+print('Первая команда:', team_1)
+print('Вторая команда:', team_2)
+print('Победители тура:', winner_list)
