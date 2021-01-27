0ade0a5889fa9b478100e49a44ec8ac6335bf28b
Mikhail <rymv@yandex.ru>
2021-01-27 16:45:21 +0300

    HW17_8
    Signed-off-by: Mikhail <rymv@yandex.ru>

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+n = int(input('Кол-во палок: '))
+k = int(input('Кол-во бросков: '))
+row = ['I' for _ in range(n)]
+for ind in range(1, k + 1):
+    print('Бросок', ind, '. Сбиты палки с номера', end=' ')
+    l_i = int(input(''))
+    print('по номер', end=' ')
+    r_i = int(input(''))
+    row[l_i - 1:r_i] = '.' * (r_i - l_i + 1)
+print(*row, sep='')
