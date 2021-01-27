bb8c3457e64c74bc4c57a356c4271818398a1274
Mikhail <rymv@yandex.ru>
2021-01-27 10:25:11 +0300

    HW17_5
    Signed-off-by: Mikhail <rymv@yandex.ru>

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+text = input('Введите строку( h минимум 2 раза): ')
+start_ind = 0
+finish_ind = 0
+first = True
+for ind in range(len(text)):
+    if text[ind] == 'h':
+        if first:
+            start_ind = ind
+            first = False
+        else:
+            finish_ind = ind
+print(text[finish_ind - 1:start_ind:-1])
