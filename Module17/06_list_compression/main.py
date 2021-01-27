a9b173af81ab8b1a616eb9a5bfad4542460f98fb
Insolente13 <lanc05@mail.ru>
2021-01-27 21:51:49 +0300

    Module 17 06 TODO

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,13 +1,19 @@
 n = int(input('Введите кол-во чисел в списке: '))
 origin_list = []
+
 for i in range(1, n + 1):
     print('Ведите', i, 'число:', end=' ')
     num = input()
     origin_list.append(num)
+
 print('Исходный список:', origin_list)
 new_list = [n for n in origin_list if n != '0']
 null_numbers = len(origin_list) - len(new_list)
 new_list += '0' * null_numbers
+
 print('"Сжатый" список:', new_list)
 new_list = new_list[:(len(new_list) - null_numbers)]
+# TODO, как удалить лементы с помощью спискового метода? =)
 print('Список без 0:', new_list)
+
+# TODO, таким образом в списках хранятся тестовые значения, как сделать числовые? =)
