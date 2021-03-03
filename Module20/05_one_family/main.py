599f9af2e069d18477fb9f081b2443720f4afab4
Mikhail <rymv@yandex.ru>
2021-03-03 09:52:07 +0300

    HW20_8 fixed 05
    Signed-off-by: Mikhail <rymv@yandex.ru>

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -12,7 +12,9 @@ inp_fem_surname = inp_surname + 'а'
 for surname_name, age in people.items():
     surname = surname_name[0]
     if surname == inp_surname or surname == inp_fem_surname:
+        print(*surname_name, age)
         total_age += age
 print('Общий возраст: ', total_age)
 
-# TODO, пожалуйста добавьте вывод ФИО и возвраста людей, чьи фамилии были указаны во вводе пользователя.
+# пожалуйста добавьте вывод ФИО и возраста людей, чьи фамилии были указаны во вводе пользователя.
+# TODO добавил. Да, неправильно прочитал условие.
