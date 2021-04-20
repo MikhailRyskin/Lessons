0a056fd4f0e6d6f550370ac5c40f0650e8e91250
Mikhail <rymv@yandex.ru>
2021-04-20 20:37:59 +0300

    HW24 1, 2

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import random
+
+
+class Warrior:
+
+    def __init__(self, number):
+        self.number = number
+        self.health = 100
+
+    def action(self, attack):
+        if attack:
+            print(f'Атаковал воин {self.number}')
+        else:
+            self.health -= 20
+            print(f'У воина {self.number} осталось {self.health} здоровья')
+
+
+choice_list = [True, False]
+unit_1 = Warrior(1)
+unit_2 = Warrior(2)
+lap = 0
+while unit_1.health > 0 and unit_2.health > 0:
+    lap += 1
+    print(f'Раунд {lap}')
+    choice = random.choice(choice_list)
+    unit_1.action(choice)
+    unit_2.action(not choice)
+    print('_' * 20)
+print(f'Игра окончена')
+if unit_1.health > 0:
+    print('Победу одержал воин 1')
+else:
+    print('Победу одержал воин 2')
--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import random
+
+
+class Student:
+
+    def __init__(self, name, group_number,  achievement):
+        self.name = name
+        self.group_number = group_number
+        self.achievement = achievement
+        self.average_score = round(sum(achievement) / len(achievement), 2)
+
+    def info(self):
+        print(f'Студент {self.name} группа {self.group_number} средний балл {self.average_score}')
+
+
+names = ['Иванов', 'Петров', 'Сидоров',  'Дзюба', 'Ловрен', 'Барриос', 'Кузяев', 'Лунёв', 'Кержаков', 'Азмун']
+students = []
+for name in names:
+    group = random.randint(100, 105)
+    points = [random.randint(2, 5) for _ in range(5)]
+    student = Student(name, group, points)
+    students.append(student)
+student_list = []
+for student in students:
+    student_list.append((student.average_score, student.name, student.group_number))
+for i in sorted(student_list):
+    print(f'студент {i[1]} группа {i[2]} средний балл {i[0]}')
